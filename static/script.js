0761f579ae5a3b2efbaf6e7bf8d041e53d51e1fa
vishaljoshi2727-rgb <vishaljoshi2727@gmail.com>
2026-07-06 13:08:51 +0530

    Add files via upload

--- static/script.js
+++ static/script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("To-Do App Loaded");
+});
 
+function filterTasks(type) {
+    let items = document.querySelectorAll("li");
+
+    items.forEach(item => {
+        if (type === "all") {
+            item.style.display = "flex";
+        } else if (type === "completed") {
+            item.style.display = item.classList.contains("done") ? "flex" : "none";
+        } else {
+            item.style.display = !item.classList.contains("done") ? "flex" : "none";
+        }
+    });
+}
